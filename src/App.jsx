8091b83afe37f33562d9110856b7860e0dc8b7be
Sandy262 <sndpkumara.p@gmail.com>
2024-07-16 01:08:15 +0530

    React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './home';
 import Aboutus from './Aboutus';
+import { Link, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="m-2 p-2 border border-info">
       <h1>India</h1>
-      <Home></Home>
-      <Aboutus></Aboutus>
+      {/* <Home></Home> */}
+      {/* <Aboutus></Aboutus> */}
+      <a href="/home">Home</a>
+      <br />
+      <Link to="/Aboutus">About us</Link>
+      <Outlet></Outlet>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Home from './home';
+import Aboutus from './Aboutus';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
+const router=createBrowserRouter([
+    // {
+    //     path:"/",
+    //     element:<div>Hello world!</div>
+    // },
+    // {
+    //     path:"/Home",
+    //     //element:<div><h1>Home Chupistha</h1><h2>Ma intlo kitchen undi</h2></div>
+    //     element:<Home></Home>
+    // },
+    // {
+    //     path:"/Aboutus",
+    //     element:<Aboutus></Aboutus>
+    // }
+    {
+        path:"/",
+        element:<App></App>,
+        children:[
+            {
+                path:"/home",
+                element:<Home></Home>
+            },
+            {
+                path:"/Aboutus",
+                element:<Aboutus></Aboutus>
+                },
+            {
+                path:"/add/:a/:b",
+                element:<h3>Parameterized routing</h3>
+            }
+        ]
+    },
+    // {
+    //     path:"/home",
+    //     element:<Home></Home>
+    // }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={router}></RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
