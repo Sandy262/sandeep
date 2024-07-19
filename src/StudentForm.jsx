99957fc0de2e169cedc65047da0d1afe8934b2c9
Sandy262 <sndpkumara.p@gmail.com>
2024-07-20 02:25:28 +0530

    FormikValidation_1

--- src/StudentForm.jsx
+++ src/StudentForm.jsx
@@ -16,13 +16,18 @@ return (
     <div className='border border-2 p-2 m-2'> 
         <h2>StudentForm</h2>
         <form onSubmit={sForm.handleSubmit}>
-        <input type="text" name='firstName' onChange={sForm.handleChange}/>
+        <input type="text" className={sForm.touched.firstName&&sForm.errors.firstName&& "border border-5 border-danger danger-outline"} name='firstName' onChange={sForm.handleChange} onBlur={sForm.handleBlur}/>
         <br />
+        {
+            sForm.touched.firstName&&sForm.errors.firstName&&<div>Please enter firstName</div>
+        }
         <input type="text" name='lastName' onChange={sForm.handleChange}/>
         <br />
         <button>Save Student</button>
         </form>
-        {JSON.stringify(sForm.errors)}
+        {/* {JSON.stringify(sForm.errors)}
+        <br />
+        {JSON.stringify(sForm)} */}
     </div>
 )
 }
