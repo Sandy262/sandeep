a156c2528fe54e01cddc958f0029d034949476e9
Sandy262 <sndpkumara.p@gmail.com>
2024-07-21 01:49:55 +0530

    GetFieldProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Child from './Child';
 import StudentForm from './StudentForm';
 function App() {
   return (
-    <div className="m-2 p-2 border border-info">
-      <StudentForm></StudentForm>
+    <div className="m-2 p-2 border border-2">
+      <h1>India</h1>
+        <StudentForm className="border border-2 m-2 p-2"></StudentForm>
+      <Child name="Sandeep">Kumar</Child>
     </div>
   );
 }
--- src/StudentForm.jsx
+++ src/StudentForm.jsx
@@ -19,8 +19,18 @@ function StudentForm() {
     })
 return (
     <div className='border border-2 p-2 m-2'> 
-        <h2>StudentForm</h2>
+        <h1>StudentForm</h1>
         <form onSubmit={sForm.handleSubmit}>
+            <h4>Gender :</h4>
+            <input type='radio' {...sForm.getFieldProps('gender')} value='male' /> Male<br />
+            <input type='radio' {...sForm.getFieldProps('gender')} value='female' /> Female<br />
+            <input type='radio' {...sForm.getFieldProps('gender')} value='others' /> Others<br />
+            <h4>Technologies to opt for :</h4>
+            <input type='checkbox' {...sForm.getFieldHelpers('Tech')} value='Javascript' /> Javascript<br />
+            <input type='checkbox' {...sForm.getFieldHelpers('Tech')} value='React.js' /> React.js<br />
+            <input type='checkbox' {...sForm.getFieldHelpers('Tech')} value='Angularjs' /> Angularjs<br />
+            <input type='checkbox' {...sForm.getFieldHelpers('Tech')} value='Redux' /> Redux<br />
+            <input type='checkbox' {...sForm.getFieldHelpers('Tech')} value='Nodejs' /> Nodejs<br />
             {
         //<input type="text" className={sForm.touched.firstName&&sForm.errors.firstName&& "border border-5 border-danger danger-outline"} name='firstName' onChange={sForm.handleChange} onBlur={sForm.handleBlur}/>
             }
