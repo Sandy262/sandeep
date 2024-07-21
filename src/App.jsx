267e0d439cfb5943d5ef777c6ac56abb68c5868f
Sandy262 <sndpkumara.p@gmail.com>
2024-07-21 06:13:23 +0530

    UseReducerHookInReact

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css';
+import { useReducer } from 'react';
 import Child from './Child';
 import StudentForm from './StudentForm';
 import { UserContext } from './UserContext';
 import A from './A'
+import { type } from '@testing-library/user-event/dist/type';
+function reducer(state,action){
+if(action.type==="INC"){
+  return {count:state.count+1}
+}
+if(action.type==="DEC"){
+  return {count:state.count-1}
+}
+}
 function App() {
+  var [state,dispatch]=useReducer(reducer,{count:0})
   return (
     <UserContext.Provider value={{name:'praveen gubbala', age:41}}><div className="m-2 p-2 border border-2 border-danger">
+    <h1>Count:{state.count}</h1>
+    <button onClick={()=>dispatch({type:"INC"})}>Increment</button>
+    <button onClick={()=>dispatch({type:"DEC"})}>Decrement</button>
     <h1>India</h1>
       <StudentForm className="border border-2 m-2 p-2"></StudentForm>
     <Child name="Sandeep">Kumar</Child>
