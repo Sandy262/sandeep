05ebfa95a0b66fd8e48cd4dff8b78ad656622433
Sandy262 <sndpkumara.p@gmail.com>
2024-07-23 22:45:49 +0530

    Redux Counter and Todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,6 +5,8 @@ function TodoList(props) {
 return (
     <div className='border border-2 m-2 p-2'>
     <h1>TodoList</h1>
+    <input type='text' id='d1'/>
+    <button onClick={()=>{props.dispatch({type:'ADD_TODO',payload:document.getElementById('d1').value})}}>Add Todo</button>
     <ul>
         {
             props.todoListR.todos.map((t)=>{
--- src/counter.jsx
+++ src/counter.jsx
@@ -5,6 +5,7 @@ function counter(props) {
 return (
     <div className="border border-2 m-2 p-2">
     <h2>Counter:{props.counterR.count}</h2>
+    <button onClick={()=>{props.dispatch({type:'INC'})}}>Increment</button>
     </div>
 )
 }
